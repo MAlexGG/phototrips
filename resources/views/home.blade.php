9f7101ff0e1df8d9d2dd8c822d2830a9a4163d4c
MAlexGG <alexandragalarzag@gmail.com>
2021-08-04 11:22:37 +0200

    home page including images

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,27 @@
-@extends('layouts.app')
+@extends('layouts.start')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <h1>Hola</h1>
-            </div>
+@section('home')
+<div class="container-fluid p-0">
+    <div class="d-flex flex-column justify-content-center align-items-center ct-cards">
+        <div class="d-flex flex-column justify-content-center align-items-center card">
+            <h2 class="txt-continent p-0">AFRICA</h2>
+            <a href=""><img class="img-card" src="{{ asset('img/africa.jpg') }}" alt="Fearscare from pixabay"></a>
+        </div>
+        <div class="d-flex flex-column justify-content-center align-items-center card">
+            <h2 class="txt-continent p-0">ASIA</h2>
+            <a href=""><img class="img-card" src="{{ asset('img/japon.jpg') }}" alt="Free-Photos from pixabay"></a>
+        </div>
+        <div class="d-flex flex-column justify-content-center align-items-center card">
+            <h2 class="txt-continent p-0">AMERICA</h2>
+            <a href=""><img class="img-card" src="{{ asset('img/newYork.jpg') }}" alt="Michael Pewney from pixabay"></a>
+        </div>
+        <div class="d-flex flex-column justify-content-center align-items-center card">
+            <h2 class="txt-continent p-0">EUROPE</h2>
+            <a href=""><img class="img-card" src="{{ asset('img/paris.jpg') }}" alt="Free-Photos from pixabay"></a>
+        </div>
+        <div class="d-flex flex-column justify-content-center align-items-center card">
+            <h2 class="txt-continent p-0">OCEANIA</h2>
+            <a href=""><img class="img-card" src="{{ asset('img/sydney.jpg') }}" alt="Patty Jansen from pixabay"></a>
         </div>
     </div>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -32,4 +32,4 @@ Route::delete('/photos/{photo}', [PhotoController::class, 'destroy'])->name('del
 
 Auth::routes();
 
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
